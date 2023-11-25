9cd4074434696a83d2f539f36da0d3822e91f4c0
Dmitri De Klerk <dmitriwarren@gmail.com>
2023-11-25 18:16:42 +0200

    Create table on dashboard for trades

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -3,8 +3,7 @@ import Modal from "../components/Modal";
 import { useEffect, useState } from "react";
 import { db } from "../config/Firebase";
 import { getDocs, collection } from "firebase/firestore";
-import Card, { TradeProps } from "../components/Card/Card";
-// import "./Dashboard.module.css";
+import { TradeProps } from "../components/Card/Card";
 
 function DashboardPage() {
 
@@ -29,14 +28,6 @@ function DashboardPage() {
 
             <div><Modal></Modal></div>
 
-            <section>
-                {trades ? (trades.map((trade, i) => (
-                    <Card {...trade} />
-                ))
-                ) : (<div>No Trade data</div>)}
-            </section>
-
-
             <div>
                 <div className="flex flex-wrap -mx-3 mb-5">
                     <div className="w-full max-w-full px-3 mb-6  mx-auto">
@@ -47,48 +38,53 @@ function DashboardPage() {
                                         <table className="w-full my-0 align-middle text-dark border-neutral-200">
                                             <thead className="align-bottom">
                                                 <tr className="font-semibold text-[0.95rem] text-secondary-dark">
-                                                    <th className="pb-3 text-start min-w-[175px]">SYMBOL</th>
+                                                    <th className="pb-3 text-start min-w-[115px]">SYMBOL</th>
                                                     <th className="pb-3 text-end min-w-[100px]">TYPE</th>
-                                                    <th className="pb-3 text-end min-w-[100px]">VOLUME</th>
-                                                    <th className="pb-3 pr-12 text-end min-w-[175px]">PROFIT</th>
+                                                    <th className="pb-3 text-end min-w-[50px]">VOLUME</th>
+                                                    <th className="pb-3 pr-12 text-end min-w-[115px]">PROFIT</th>
                                                     <th className="pb-3 pr-12 text-end min-w-[100px]">DATE</th>
                                                     <th className="pb-3 text-end min-w-[50px]">DETAILS</th>
                                                 </tr>
                                             </thead>
                                             <tbody>
-                                                <tr className="border-b border-dashed last:border-b-0">
-                                                    <td className="p-3 pl-0">
-                                                        <div className="flex items-center">
-                                                            <div className="flex flex-col justify-start">
-                                                                <span className="mb-1 font-semibold transition-colors duration-200 ease-in-out text-lg/normal text-secondary-inverse hover:text-primary"> Social Media API </span>
-                                                            </div>
-                                                        </div>
-                                                    </td>
-                                                    <td className="p-3 pr-0 text-end">
-                                                        <span className="font-semibold text-light-inverse text-md/normal">Olivia Cambell</span>
-                                                    </td>
-                                                    <td className="p-3 pr-0 text-end">
-                                                        <span className="text-center align-baseline inline-flex px-2 py-1 mr-auto items-center font-semibold text-base/none text-success bg-success-light rounded-lg">
-                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 mr-1">
-                                                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
-                                                            </svg> 6.5% </span>
-                                                    </td>
-                                                    <td className="p-3 pr-12 text-end">
-                                                        <span className="text-center align-baseline inline-flex px-4 py-3 mr-auto items-center font-semibold text-[.95rem] leading-none text-primary bg-primary-light rounded-lg"> In Progress </span>
-                                                    </td>
-                                                    <td className="pr-0 text-start">
-                                                        <span className="font-semibold text-light-inverse text-md/normal">2023-08-23</span>
-                                                    </td>
-                                                    <td className="p-3 pr-0 text-end">
-                                                        <button className="ml-auto relative text-secondary-dark bg-light-dark hover:text-primary flex items-center h-[25px] w-[25px] text-base font-medium leading-normal text-center align-middle cursor-pointer rounded-2xl transition-colors duration-200 ease-in-out shadow-none border-0 justify-center">
-                                                            <span className="flex items-center justify-center p-0 m-0 leading-none shrink-0 ">
-                                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4">
-                                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-                                                                </svg>
-                                                            </span>
-                                                        </button>
-                                                    </td>
-                                                </tr>
+
+
+                                                {trades ? (
+                                                    trades.map((trade) => (
+
+                                                        <tr className="border-b border-dashed last:border-b-0">
+                                                            <td className="p-3 pl-0">
+                                                                <div className="flex items-center">
+                                                                    <div className="flex flex-col justify-start">
+                                                                        <span className="mb-1 font-semibold transition-colors duration-200 ease-in-out text-lg/normal text-secondary-inverse hover:text-primary"> {trade.symbol} </span>
+                                                                    </div>
+                                                                </div>
+                                                            </td>
+                                                            <td className="p-3 pr-0 text-end">
+                                                                <span className="font-semibold text-light-inverse text-md/normal">{trade.type}</span>
+                                                            </td>
+                                                            <td className="p-3 pr-0 text-end">
+                                                                <span className="text-center align-baseline inline-flex px-2 py-1 mr-auto items-center font-semibold text-base/none text-success bg-success-light rounded-lg"> {trade.volume} </span>
+                                                            </td>
+                                                            <td className="p-3 pr-12 text-end">
+                                                                <span className="text-center align-baseline inline-flex px-4 py-3 mr-auto items-center font-semibold text-[.95rem] leading-none text-primary bg-primary-light rounded-lg"> {trade.profit} </span>
+                                                            </td>
+                                                            <td className="pr-0 text-start">
+                                                                <span className="font-semibold text-light-inverse text-md/normal">{trade.date}</span>
+                                                            </td>
+                                                            <td className="p-3 pr-0 text-end">
+                                                                <button className="ml-auto relative text-secondary-dark bg-light-dark hover:text-primary flex items-center h-[25px] w-[25px] text-base font-medium leading-normal text-center align-middle cursor-pointer rounded-2xl transition-colors duration-200 ease-in-out shadow-none border-0 justify-center">
+                                                                    <span className="flex items-center justify-center p-0 m-0 leading-none shrink-0 ">
+                                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4">
+                                                                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                                                                        </svg>
+                                                                    </span>
+                                                                </button>
+                                                            </td>
+                                                        </tr>
+                                                    ))
+                                                ) : (<div>No Trade data</div>)}
+
                                             </tbody>
                                         </table>
                                     </div>
